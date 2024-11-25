c4e5455552cfc14e5739073912306f7889622f51
Maihuy211 <maihuylc@gmail.com>
2024-11-26 02:13:56 +0700

    upload baitap5

---
+++ baitap5.c
@@ -0,0 +1,13 @@
+#include<stdio.h> 
+int main(){
+  int i,j,result;
+     for(i=1;i<=9;i++){
+	      printf("bang nhan %d\n",i);
+	 for(j=1;j<=10;j++){
+	    result=i*j;
+        printf("%d * %d = %d\n",i,j,result);
+            }            
+            }
+			return 0;
+         
+	}
