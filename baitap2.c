366f5a58caeaee018442e5daebc5cbedb52610cf
Maihuy211 <maihuylc@gmail.com>
2024-11-26 02:13:05 +0700

    upload baitap2

---
+++ baitap2.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+int main(){
+   int so_cho_truoc=21,so_nhap_vao;
+    if(so_nhap_vao>0){
+	
+	do{
+    printf("nhap mot so: ");
+   	scanf("%d",&so_nhap_vao);
+	}while(so_cho_truoc != so_nhap_vao);
+	  	printf("so dc nhap trung voi so cho truoc");
+	  	
+	}   
+	 else{
+	 	printf("loi");
+	 }
+	
+
+return 0;
+	}
